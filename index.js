9ca39b042f27f209b9d07d1ceb238a6a06463113
Md Emon Hossain <96044760+S1Emon@users.noreply.github.com>
2022-05-29 17:26:41 +0600

    single data load

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,6 +32,12 @@ async function run() {
             const query = {}
             const result = await partsCollection.find(query).toArray()
             res.send(result);
+        })
+        app.get("/part/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const part = await partsCollection.findOne(query);
+            res.send(part)
         })
         app.get("/new", async (req, res) => {
             const query = {}
